a2684795c2b813d5e8fcf39a0498cefed6ad011d
baris menderes <bmenderes@gmail.com>
2022-09-17 14:11:39 +0300

    js first init

---
+++ js/02-degisken-tanimlama.js
@@ -0,0 +1,37 @@
+// **** let ve const ile degisken tanimalama *****
+
+// var ile degisken tanimalama:
+// var serverName = "kolduyoruz.org"
+// console.log(serverName);
+
+
+// let ile degiskeni bos tanimlamak:
+let serverName;
+console.log(serverName);
+
+// let ile degiskene bilgi atamak
+serverName = "https://kodluyoruz.org"
+console.log(serverName);
+
+// let ile degiskene bilgiyi aktararak tanimlamak:
+let password = "1234";
+console.log(password);
+
+//degisken atamasi yapmadan once kullanmaya calismak
+/* HATALI KULLANIM
+console.log(fullName)
+let fullName = "Baris Menderes"; */ 
+let fullName = "Baris Menderes";
+console.log(fullName);
+
+// let ile taanimlanan bilgiyi guncelleneme
+
+fullName = fullName + " " + "Mahmut"
+console.log(fullName);
+
+//const ile degiskeni bos tanimlamaya calismak
+const SERVER_PASSWORD = "1234z<c<c";
+console.log(SERVER_PASSWORD);
+
+// const ile degisken tanimlamak
+
